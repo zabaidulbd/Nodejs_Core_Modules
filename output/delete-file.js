311351352406e9665cf75cb27541d9a7b927bd8f
Md. Zabaidul Islam <zislam5622@gmail.com>
2025-11-23 15:16:41 +0600

    how to delete file asyncronously

--- output/delete-file.js
+++ output/delete-file.js
@@ -1,10 +1,23 @@
 const fs = require("fs");
 
-fs.writeFileSync("./temp.txt", "this is a temp file");
-console.log("temp file created");
+// fs.writeFileSync("./temp.txt", "this is a temp file");
+// console.log("temp file created");
 
-if (fs.existsSync("./temp.txt")) {
-  console.log("file exits");
-  fs.unlinkSync("./temp.txt");
-  console.log("file deleted");
-}
+// if (fs.existsSync("./temp.txt")) {
+//   console.log("file exits");
+//   fs.unlinkSync("./temp.txt");
+//   console.log("file deleted");
+// }
+
+// delete any file asycronous ly....
+
+fs.writeFile("./temp2.txt", "this is another temp file", (err) => {
+  console.log("another temp file created");
+  fs.unlink("./temp2.txt", (err) => {
+    if (err) {
+      console.error("Error:", err.message);
+    } else {
+      console.log("temp 2 deleted");
+    }
+  });
+});
